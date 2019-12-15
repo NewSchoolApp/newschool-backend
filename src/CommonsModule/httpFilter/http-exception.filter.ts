4bfb61211bc01462ca1311116e6ddcef3d7fa9ba
Andre <andeluisflor@live.com>
2019-12-15 17:27:54 -0300

    adicionado rollbar ao projeto

---
+++ src/CommonsModule/httpFilter/http-exception.filter.ts
@@ -0,0 +1,25 @@
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
+import { Response } from 'express';
+import * as Rollbar from 'rollbar'
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+    catch(exception: HttpException, host: ArgumentsHost) {
+        
+        const ctx = host.switchToHttp();
+        const response = ctx.getResponse<Response>();
+        const status = exception.getStatus();
+
+        const rollbar = new Rollbar({
+            accessToken: process.env.ROLLBAR_TOKEN,
+            captureUncaught: true,
+            captureUnhandledRejections: true
+          });
+                
+        const objResponse = exception.getResponse();
+        
+        rollbar.log(JSON.stringify(objResponse));
+
+        response.status(status).json(objResponse);
+    }
+}
