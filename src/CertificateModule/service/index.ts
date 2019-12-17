2896e05fb51be2f256e6aca7421f165f3070f549
Davi de Carvalho <davi_carvalho96@hotmail.com>
2019-12-17 18:04:52 -0300

    Arrumando testes

---
+++ src/CertificateModule/service/index.ts
@@ -0,0 +1,1 @@
+export { CertificateService } from './certificate.service';
--- src/CommonsModule/constants.ts
+++ src/CommonsModule/constants.ts
@@ -4,6 +4,7 @@ export class Constants {
 
   static OAUTH_ENDPOINT = 'oauth';
   static USER_ENDPOINT = 'user';
+  static CERTIFICATE_ENDPOINT = 'certificate';
   static COURSE_ENDPOINT = 'course';
 
 }
