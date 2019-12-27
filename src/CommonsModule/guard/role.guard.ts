4602dc9f578c92eb2e54f757032287767a148be2
Davi de Carvalho <davi_carvalho96@hotmail.com>
2019-12-27 13:27:11 -0300

    Retornando erro de jwt expirado no guarda

--- src/CommonsModule/guard/role.guard.ts
+++ src/CommonsModule/guard/role.guard.ts
@@ -1,4 +1,4 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { RoleEnum } from '../../SecurityModule/enum';
 import { User } from '../../UserModule/entity';
@@ -20,14 +20,21 @@ export class RoleGuard implements CanActivate {
 
     const request = context.switchToHttp()
       .getRequest();
-    
+
     const authorizationHeader = request.headers.authorization;
 
     if (!authorizationHeader)
       return false;
-      
+
     const [, token] = authorizationHeader.split(' ');
-    const user: User = this.jwtService.verify(token);
+    let user: User;
+    try {
+      user = this.jwtService.verify<User>(token);
+    } catch (e) {
+      if (e.name === 'TokenExpiredError') {
+        throw new UnauthorizedException(e.message)
+      }
+    }
     const hasPermission: boolean = roles
       .some((role) => role === user.role.name);
     return user?.role && hasPermission;
