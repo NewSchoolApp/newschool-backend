116c2e131ad2aeb48fd89fe348f69be5e3e3a89a
ericguim <eric.guimaraes07@gmail.com>
2020-01-09 06:46:16 +0000

    Corrigindo entity, dto

--- src/CourseModule/dto/new-test.dto.ts
+++ src/CourseModule/dto/new-test.dto.ts
@@ -1,36 +1,42 @@
 import { Test } from '../entity';
 import { ApiModelProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsString, IsNotEmpty } from 'class-validator';
 import { Expose } from 'class-transformer';
 
 export class NewTestDTO {
     @IsString()
     @Expose()
+    @IsNotEmpty()
     @ApiModelProperty({ type: String })
     title: Test['title'];
 
     @IsString()
     @Expose()
+    @IsNotEmpty()
     @ApiModelProperty({ type: String })
     correctAlternative: Test['correctAlternative'];
 
     @IsString()
     @Expose()
+    @IsNotEmpty()
     @ApiModelProperty({ type: String })
     firstAlternative: Test['firstAlternative'];
 
     @IsString()
     @Expose()
+    @IsNotEmpty()
     @ApiModelProperty({ type: String })
     secondAlternative: Test['secondAlternative'];
 
     @IsString()
     @Expose()
+    @IsNotEmpty()
     @ApiModelProperty({ type: String })
     thirdAlternative: Test['thirdAlternative'];
 
     @IsString()
     @Expose()
+    @IsNotEmpty()
     @ApiModelProperty({ type: String })
     fourthAlternative: Test['fourthAlternative'];
 
@@ -41,6 +47,7 @@ export class NewTestDTO {
 
     @IsString()
     @Expose()
+    @IsNotEmpty()
     @ApiModelProperty({ type: String })
     part: Test['part'];
 }
--- src/CourseModule/entity/test.entity.ts
+++ src/CourseModule/entity/test.entity.ts
@@ -7,51 +7,60 @@ import { Expose } from 'class-transformer';
 @Unique(['nextTest', 'part'])
 @Entity()
 export class Test extends Audit{
+    @Expose()
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
+    @Expose()
     @Column({
         nullable: false,
         name: 'title'
     })
     title: string;
 
+    @Expose()
     @Column({
         nullable: false,
         name: 'correct_alternative'
     })
     correctAlternative: string;
 
+    @Expose()
     @Column({
         nullable: false,
         name: 'first_alternative'
     })
     firstAlternative: string;
 
+    @Expose()
     @Column({
         nullable: false,
         name: 'second_alternative'
     })
     secondAlternative: string;
 
+    @Expose()
     @Column({
         nullable: false,
         name: 'third_alternative'
     })
     thirdAlternative: string;
 
+    @Expose()
     @Column({
         nullable: false,
         name: 'fourth_alternative'
     })
     fourthAlternative: string;
 
+    @Expose()
     @Column({
         nullable: true,
         name: 'nxt_tst_id'
     })
     nextTest: string;
 
+    @Expose()
     @ManyToOne<Part>(() => Part, (part: Part) => part.tests)
     @JoinColumn({
         name: 'part_id'
