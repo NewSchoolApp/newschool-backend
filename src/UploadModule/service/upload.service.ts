ade49a25fb0b9f3b8592f7d9ffeeff6be7334539
Petherson Moreno <petherson.moreno@ingaia.com.br>
2020-01-09 13:31:13 -0300

    refactor: applied use of promisify in UploadService

--- src/UploadModule/service/upload.service.ts
+++ src/UploadModule/service/upload.service.ts
@@ -1,53 +1,45 @@
 import * as fs from 'fs';
+import { promisify } from 'util';
 import { Response } from 'express';
-import { Injectable, NotFoundException, InternalServerErrorException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+
+const fileAccess: (
+  path: string,
+  mode: number | undefined,
+) => Promise<void> = promisify(fs.access);
 
 @Injectable()
 export class UploadService {
-    public async sendFile(
-        fileName: string,
-        response: Response,
-    ): Promise<void> {
-        await this.ensureFileExists(fileName);
-        await this.sendFileToResponse(fileName, response);
-    }
+  public async sendFile(fileName: string, response: Response): Promise<void> {
+    await this.ensureFileExists(fileName);
+    await this.sendFileToResponse(fileName, response);
+  }
 
-    private async ensureFileExists(fileName: string): Promise<void> {
-      try {
-          await this.fileExists(
-              `${__dirname}/../../../upload/${fileName}`,
-          );
-      } catch (error) {
-          throw new NotFoundException(`File '${fileName}' not found`);
-      }
+  private async ensureFileExists(fileName: string): Promise<void> {
+    try {
+      const path = `${__dirname}/../../../upload/${fileName}`;
+      await fileAccess(path, fs.constants.F_OK);
+    } catch (error) {
+      throw new NotFoundException(`File '${fileName}' not found`);
     }
+  }
 
-    private async sendFileToResponse(
-            fileName: string,
-            response: Response,
-        ): Promise<void> {
-        return new Promise((resolve, reject) => {
-            response.sendfile(
-                fileName,
-                { root: 'upload' },
-                (error: Error) => {
-                    if (error) {
-                        reject(new InternalServerErrorException(error.message));
-                    }
-                    resolve();
-                },
-            );
-        });
+  private async sendFileToResponse(
+    fileName: string,
+    response: Response,
+  ): Promise<void> {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const sendFile: (path: string, options: any) => Promise<void> = promisify(
+      response.sendfile.bind(response),
+    );
+    try {
+      await sendFile(fileName, { root: 'upload' });
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
     }
-    private fileExists(path: string): Promise<void> {
-        return new Promise((resolve, reject) => {
-            fs.access(path, fs.constants.F_OK, (error) => {
-                if (error) {
-                    reject(error);
-                    return;
-                }
-                resolve();
-            });
-        });
-    }
+  }
 }
