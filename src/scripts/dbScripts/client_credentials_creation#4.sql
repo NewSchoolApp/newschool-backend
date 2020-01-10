01b5fc3bcb8c23c7c2df0f315f0cfca54229c282
Davi de Carvalho <davi_carvalho96@hotmail.com>
2020-01-10 10:52:46 -0300

    Criado constraints para unique e alterado colunas para enum

--- src/scripts/dbScripts/client_credentials_creation#4.sql
+++ src/scripts/dbScripts/client_credentials_creation#4.sql
@@ -1,12 +1,13 @@
-CREATE TABLE `client-credentials` (
+create TABLE `client-credentials` (
   `createdAt` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
   `updatedAt` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
   `version` int(11) NOT NULL,
   `id` varchar(36) NOT NULL,
-  `name` varchar(255) NOT NULL,
+  `name` ENUM('NEWSCHOOL@EXTERNAL', 'NEWSCHOOL@FRONT') NOT NULL,
   `secret` varchar(255) NOT NULL,
   `roleId` varchar(36) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_CLNT_CRDTLS` (`roleId`),
-  CONSTRAINT `FK_CLNT_CRDTLS` FOREIGN KEY (`roleId`) REFERENCES `role` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `FK_CLNT_CRDTLS` FOREIGN KEY (`roleId`) REFERENCES `role` (`id`) ON delete NO ACTION ON update NO ACTION,
+  CONSTRAINT IDX_c6528d2d9ab22e6802915fd9f9 UNIQUE (name)
 );
