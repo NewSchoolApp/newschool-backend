d3c38f6b341c2b38f6d5c10801c5dc0868fb61a4
Petherson Moreno <pethermoreno@gmail.com>
2020-01-13 21:02:00 -0300

    refactor: updated identation

--- src/CommonsModule/constants.ts
+++ src/CommonsModule/constants.ts
@@ -1,14 +1,14 @@
 export class Constants {
-    static API_PREFIX = 'api';
-    static API_VERSION_1 = 'v1';
-    static OAUTH_ENDPOINT = 'oauth';
-    static USER_ENDPOINT = 'user';
-    static CERTIFICATE_ENDPOINT = 'certificate';
-    static COURSE_ENDPOINT = 'course';
-    static MESSAGE_ENDPOINT = 'message';
-    static LESSON_ENDPOINT = 'lesson';
-    static PART_ENDPOINT = 'part';
-    static COURSE_TAKEN_ENDPOINT = 'courseTaken';
-    static UPLOAD_ENDPOINT = 'upload';
-    static TEST_ENDPOINT = 'test';
+  static API_PREFIX = 'api';
+  static API_VERSION_1 = 'v1';
+  static OAUTH_ENDPOINT = 'oauth';
+  static USER_ENDPOINT = 'user';
+  static CERTIFICATE_ENDPOINT = 'certificate';
+  static COURSE_ENDPOINT = 'course';
+  static MESSAGE_ENDPOINT = 'message';
+  static LESSON_ENDPOINT = 'lesson';
+  static PART_ENDPOINT = 'part';
+  static COURSE_TAKEN_ENDPOINT = 'courseTaken';
+  static UPLOAD_ENDPOINT = 'upload';
+  static TEST_ENDPOINT = 'test';
 }
