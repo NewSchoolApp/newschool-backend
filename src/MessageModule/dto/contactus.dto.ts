3252afd9f6b0fe355bd432868a62cacff4960572
Davi de Carvalho <davi_carvalho96@hotmail.com>
2020-01-24 09:42:57 -0300

    Alterado DTO par adicionar celular e email ser opcional

--- src/MessageModule/dto/contactus.dto.ts
+++ src/MessageModule/dto/contactus.dto.ts
@@ -1,19 +1,29 @@
-import { ApiModelProperty } from '@nestjs/swagger';
-import { IsNotEmpty } from 'class-validator';
+import { ApiModelProperty, ApiModelPropertyOptional } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { Expose } from 'class-transformer';
 
 export class ContactUsDTO {
   @ApiModelProperty({ type: String })
   @IsNotEmpty()
+  @IsString()
   @Expose()
   name: string;
 
   @ApiModelProperty({ type: String })
   @IsNotEmpty()
+  @IsString()
   @Expose()
-  email: string;
+  cellphone: string;
 
   @ApiModelProperty({ type: String })
+  @IsNotEmpty()
+  @IsString()
   @Expose()
   message: string;
+
+  @ApiModelPropertyOptional({ type: String })
+  @IsOptional()
+  @IsString()
+  @Expose()
+  email: string;
 }
