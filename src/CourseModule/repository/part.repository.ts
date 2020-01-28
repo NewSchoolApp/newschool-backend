e7c4c04f214ceffd43fba1d09feed71ecbfa671f
Davi de Carvalho <davi_carvalho96@hotmail.com>
2020-01-28 09:04:31 -0300

    Arrumado tipagens do repository

--- src/CourseModule/repository/part.repository.ts
+++ src/CourseModule/repository/part.repository.ts
@@ -1,17 +1,16 @@
 import { EntityRepository, Repository } from 'typeorm';
-import { Part } from '../entity';
+import { Lesson, Part } from '../entity';
 
 @EntityRepository(Part)
 export class PartRepository extends Repository<Part> {
-  async findByTitle({ title }): Promise<Part | undefined> {
-    return this.findOne({ title });
-  }
-
-  async findById({ id }): Promise<Part | undefined> {
+  async findById(id: string): Promise<Part | undefined> {
     return this.findOne({ id });
   }
 
-  async findByTitleAndLessonId({ title, lesson }): Promise<Part | undefined> {
+  async findByTitleAndLesson(
+    title: string,
+    lesson: Lesson,
+  ): Promise<Part | undefined> {
     return this.findOne({ title, lesson });
   }
 }
