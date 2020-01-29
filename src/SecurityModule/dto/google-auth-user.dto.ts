a6974a615b41f2c66298d799be79979164b64363
Davi de Carvalho <davi_carvalho96@hotmail.com>
2020-01-29 09:21:30 -0300

    Removido propriedades opcionais do body no oauth de social media

--- src/SecurityModule/dto/google-auth-user.dto.ts
+++ src/SecurityModule/dto/google-auth-user.dto.ts
@@ -1,36 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class GoogleAuthUserDTO {
   @ApiProperty({ type: String })
-  @IsString()
-  email: string;
-
-  @ApiProperty({ type: String })
-  @IsString()
-  email_verified: boolean;
-
-  @ApiProperty({ type: String })
-  @IsString()
-  family_name: string;
-
-  @ApiProperty({ type: String })
-  @IsString()
-  given_name: string;
-
-  @ApiProperty({ type: String })
-  @IsString()
-  locale: string;
-
-  @ApiProperty({ type: String })
-  @IsString()
-  name: string;
-
-  @ApiProperty({ type: String })
-  @IsString()
-  picture: string;
-
-  @ApiProperty({ type: String })
+  @IsNotEmpty()
   @IsString()
   sub: string;
+
+  @ApiProperty({ type: String })
+  @IsNotEmpty()
+  @IsString()
+  email: string;
 }
