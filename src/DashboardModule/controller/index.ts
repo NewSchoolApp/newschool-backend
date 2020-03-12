0da6b5b790fb3026b1a31d3f7f365aa95d4fe005
Davi de Carvalho <davi_carvalho96@hotmail.com>
2020-03-11 21:55:56 -0300

    Revert "Adding dashboard module"

--- src/DashboardModule/controller/index.ts
+++
@@ -1,1 +0,0 @@
-export { DashboardController } from './dashboard.controller';
--- src/DashboardModule/dashboard.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DashboardService } from './service';
-import { DashboardController } from './controller';
-
-@Module({
-  controllers: [DashboardController],
-  exports: [DashboardService],
-})
-export class DashboardModule {}
