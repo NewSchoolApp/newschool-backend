623a82fc4ea9d12b891775c7a85471a1dfbf9f29
Davi de Carvalho <davi_carvalho96@hotmail.com>
2020-08-01 15:03:45 -0300

    feat: adding security

--- src/DashboardModule/controller/dashboard.controller.ts
+++ src/DashboardModule/controller/dashboard.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Query } from '@nestjs/common';
-import { Constants } from '../../CommonsModule';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { Constants, NeedRole, RoleGuard } from '../../CommonsModule';
 import { DashboardService } from '../service/dashboard.service';
 import { UserStatusEnum } from '../enum/UserStatusEnum';
+import { RoleEnum } from '../../SecurityModule/enum/role.enum';
 
 @Controller(
   `${Constants.API_PREFIX}/${Constants.API_VERSION_1}/${Constants.DASHBOARD_ENDPOINT}`,
@@ -10,6 +11,8 @@ export class DashboardController {
   constructor(private service: DashboardService) {}
 
   @Get('/user/quantity')
+  @UseGuards(RoleGuard)
+  @NeedRole(RoleEnum.ADMIN)
   public async getUserQuantity(
     @Query('status') status?: UserStatusEnum,
   ): Promise<number> {
