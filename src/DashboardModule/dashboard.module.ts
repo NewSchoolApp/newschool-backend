b0574f93f460fc14995ee69d0c02f225d0f15e0e
Davi de Carvalho <davi_carvalho96@hotmail.com>
2020-08-01 16:23:33 -0300

    fix: added jwtmodule to dashboard service

--- src/DashboardModule/dashboard.module.ts
+++ src/DashboardModule/dashboard.module.ts
@@ -3,9 +3,24 @@ import { DashboardService } from './service/dashboard.service';
 import { DashboardController } from './controller/dashboard.controller';
 import { UserModule } from '../UserModule/user.module';
 import { CourseTakenModule } from '../CourseTakenModule/course.taken.module';
+import { SecurityModule } from '../SecurityModule/security.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, CourseTakenModule],
+  imports: [
+    JwtModule.registerAsync({
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<number>('EXPIRES_IN_ACCESS_TOKEN'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    UserModule,
+    CourseTakenModule,
+  ],
   controllers: [DashboardController],
   providers: [DashboardService],
 })
