2a7f04a167c5ed21aeb5805ee3eb207c2e718c4a
Davi de Carvalho <davi_carvalho96@hotmail.com>
2020-08-05 15:10:12 -0300

    fix: catching errors on beforeAll

--- test/DashboardModule/dashboard.e2e-spec.ts
+++ test/DashboardModule/dashboard.e2e-spec.ts
@@ -49,144 +49,148 @@ describe('DashboardController (e2e)', () => {
   let dbConnection: Connection;
 
   beforeAll(async () => {
-    moduleFixture = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+    try {
+      moduleFixture = await Test.createTestingModule({
+        imports: [AppModule],
+      }).compile();
 
-    initializeTransactionalContext();
-    app = moduleFixture.createNestApplication();
-    app.useGlobalPipes(new ValidationPipe());
-    await app.init();
+      initializeTransactionalContext();
+      app = moduleFixture.createNestApplication();
+      app.useGlobalPipes(new ValidationPipe());
+      await app.init();
 
-    dbConnection = moduleFixture.get(Connection);
+      dbConnection = moduleFixture.get(Connection);
 
-    courseRepository = moduleFixture.get<Repository<Course>>(
-      getRepositoryToken(Course),
-    );
-    userRepository = moduleFixture.get<Repository<User>>(
-      getRepositoryToken(User),
-    );
-    courseTakenRepository = moduleFixture.get<Repository<CourseTaken>>(
-      getRepositoryToken(CourseTaken),
-    );
+      courseRepository = moduleFixture.get<Repository<Course>>(
+        getRepositoryToken(Course),
+      );
+      userRepository = moduleFixture.get<Repository<User>>(
+        getRepositoryToken(User),
+      );
+      courseTakenRepository = moduleFixture.get<Repository<CourseTaken>>(
+        getRepositoryToken(CourseTaken),
+      );
 
-    const userService: UserService = moduleFixture.get<UserService>(
-      UserService,
-    );
+      const userService: UserService = moduleFixture.get<UserService>(
+        UserService,
+      );
 
-    const courseService: CourseService = moduleFixture.get<CourseService>(
-      CourseService,
-    );
+      const courseService: CourseService = moduleFixture.get<CourseService>(
+        CourseService,
+      );
 
-    const courseTakenService: CourseTakenService = moduleFixture.get<
-      CourseTakenService
-    >(CourseTakenService);
+      const courseTakenService: CourseTakenService = moduleFixture.get<
+        CourseTakenService
+      >(CourseTakenService);
 
-    const roleRepository: Repository<Role> = moduleFixture.get<
-      Repository<Role>
-    >(getRepositoryToken(Role));
-    let roleAdmin = await roleRepository.findOne({ name: RoleEnum.ADMIN });
-    if (!roleAdmin) {
-      const role: Role = new Role();
-      role.name = RoleEnum.ADMIN;
-      roleAdmin = await roleRepository.save(role);
+      const roleRepository: Repository<Role> = moduleFixture.get<
+        Repository<Role>
+      >(getRepositoryToken(Role));
+      let roleAdmin = await roleRepository.findOne({ name: RoleEnum.ADMIN });
+      if (!roleAdmin) {
+        const role: Role = new Role();
+        role.name = RoleEnum.ADMIN;
+        roleAdmin = await roleRepository.save(role);
+      }
+
+      const clientCredentialRepository: Repository<ClientCredentials> = moduleFixture.get<
+        Repository<ClientCredentials>
+      >(getRepositoryToken(ClientCredentials));
+      let clientCredentials = await clientCredentialRepository.findOne({
+        name: ClientCredentialsEnum['NEWSCHOOL@FRONT'],
+      });
+      if (!clientCredentials) {
+        clientCredentials = new ClientCredentials();
+        clientCredentials.name = ClientCredentialsEnum['NEWSCHOOL@FRONT'];
+        clientCredentials.secret = 'test2';
+        clientCredentials.role = roleAdmin;
+        await clientCredentialRepository.save(clientCredentials);
+      }
+      authorization = stringToBase64(
+        `${clientCredentials.name}:${clientCredentials.secret}`,
+      );
+
+      const newUser: NewUserDTO = {
+        email: 'my-user1@email.com',
+        password: 'mypass',
+        urlInstagram: 'instagram',
+        urlFacebook: 'facebook',
+        name: 'name',
+        nickname: 'random nickname',
+        gender: GenderEnum.MALE,
+        schooling: EscolarityEnum.ENSINO_FUNDAMENTAL_COMPLETO,
+        profession: 'random profession',
+        birthday: new Date(),
+        address: 'random adress',
+        institutionName: 'random institution',
+        role: adminRoleEnum,
+      };
+      const addedUser = await userService.add(newUser);
+
+      const newUser2: NewUserDTO = {
+        email: 'my-user2@email.com',
+        password: 'mypass',
+        urlInstagram: 'instagram',
+        urlFacebook: 'facebook',
+        name: 'name',
+        nickname: 'random nickname',
+        gender: GenderEnum.MALE,
+        schooling: EscolarityEnum.ENSINO_FUNDAMENTAL_COMPLETO,
+        profession: 'random profession',
+        birthday: new Date(),
+        address: 'random adress',
+        institutionName: 'random institution',
+        role: adminRoleEnum,
+      };
+      const addedUser2 = await userService.add(newUser2);
+
+      const newCourse: NewCourseDTO = {
+        title: 'Teste coursetaken E2E to add',
+        thumbUrl: 'http://teste.com/thumb.png',
+        authorName: 'Test',
+        authorDescription: 'Test description',
+        description: 'Este é um registro de teste',
+        workload: 1,
+      };
+      const addedCourse = await courseService.add(newCourse, {
+        fileName: 'teste',
+      });
+
+      const newCourse2: NewCourseDTO = {
+        title: 'Teste coursetaken E2E to add 2',
+        thumbUrl: 'http://teste.com/thumb.png',
+        authorName: 'Test',
+        authorDescription: 'Test description',
+        description: 'Este é um registro de teste',
+        workload: 1,
+      };
+      const addedCourse2 = await courseService.add(newCourse2, {
+        fileName: 'teste',
+      });
+
+      const newCourseTaken = {
+        userId: addedUser.id,
+        courseId: addedCourse.id,
+      };
+      await courseTakenService.add(newCourseTaken);
+
+      const newCourseTaken2 = {
+        userId: addedUser.id,
+        courseId: addedCourse2.id,
+      };
+      await courseTakenService.add(newCourseTaken2);
+
+      const newCourseTaken3 = {
+        userId: addedUser2.id,
+        courseId: addedCourse2.id,
+      };
+      await courseTakenService.add(newCourseTaken3);
+
+      await courseTakenService.advanceOnCourse(addedUser.id, addedCourse2.id);
+      await courseTakenService.advanceOnCourse(addedUser2.id, addedCourse2.id);
+    } catch (e) {
+      console.log(JSON.stringify(e));
     }
-
-    const clientCredentialRepository: Repository<ClientCredentials> = moduleFixture.get<
-      Repository<ClientCredentials>
-    >(getRepositoryToken(ClientCredentials));
-    let clientCredentials = await clientCredentialRepository.findOne({
-      name: ClientCredentialsEnum['NEWSCHOOL@FRONT'],
-    });
-    if (!clientCredentials) {
-      clientCredentials = new ClientCredentials();
-      clientCredentials.name = ClientCredentialsEnum['NEWSCHOOL@FRONT'];
-      clientCredentials.secret = 'test2';
-      clientCredentials.role = roleAdmin;
-      await clientCredentialRepository.save(clientCredentials);
-    }
-    authorization = stringToBase64(
-      `${clientCredentials.name}:${clientCredentials.secret}`,
-    );
-
-    const newUser: NewUserDTO = {
-      email: 'my-user1@email.com',
-      password: 'mypass',
-      urlInstagram: 'instagram',
-      urlFacebook: 'facebook',
-      name: 'name',
-      nickname: 'random nickname',
-      gender: GenderEnum.MALE,
-      schooling: EscolarityEnum.ENSINO_FUNDAMENTAL_COMPLETO,
-      profession: 'random profession',
-      birthday: new Date(),
-      address: 'random adress',
-      institutionName: 'random institution',
-      role: adminRoleEnum,
-    };
-    const addedUser = await userService.add(newUser);
-
-    const newUser2: NewUserDTO = {
-      email: 'my-user2@email.com',
-      password: 'mypass',
-      urlInstagram: 'instagram',
-      urlFacebook: 'facebook',
-      name: 'name',
-      nickname: 'random nickname',
-      gender: GenderEnum.MALE,
-      schooling: EscolarityEnum.ENSINO_FUNDAMENTAL_COMPLETO,
-      profession: 'random profession',
-      birthday: new Date(),
-      address: 'random adress',
-      institutionName: 'random institution',
-      role: adminRoleEnum,
-    };
-    const addedUser2 = await userService.add(newUser2);
-
-    const newCourse: NewCourseDTO = {
-      title: 'Teste coursetaken E2E to add',
-      thumbUrl: 'http://teste.com/thumb.png',
-      authorName: 'Test',
-      authorDescription: 'Test description',
-      description: 'Este é um registro de teste',
-      workload: 1,
-    };
-    const addedCourse = await courseService.add(newCourse, {
-      fileName: 'teste',
-    });
-
-    const newCourse2: NewCourseDTO = {
-      title: 'Teste coursetaken E2E to add 2',
-      thumbUrl: 'http://teste.com/thumb.png',
-      authorName: 'Test',
-      authorDescription: 'Test description',
-      description: 'Este é um registro de teste',
-      workload: 1,
-    };
-    const addedCourse2 = await courseService.add(newCourse2, {
-      fileName: 'teste',
-    });
-
-    const newCourseTaken = {
-      userId: addedUser.id,
-      courseId: addedCourse.id,
-    };
-    await courseTakenService.add(newCourseTaken);
-
-    const newCourseTaken2 = {
-      userId: addedUser.id,
-      courseId: addedCourse2.id,
-    };
-    await courseTakenService.add(newCourseTaken2);
-
-    const newCourseTaken3 = {
-      userId: addedUser2.id,
-      courseId: addedCourse2.id,
-    };
-    await courseTakenService.add(newCourseTaken3);
-
-    await courseTakenService.advanceOnCourse(addedUser.id, addedCourse2.id);
-    await courseTakenService.advanceOnCourse(addedUser2.id, addedCourse2.id);
   });
 
   it('should have 2 as quantity of certificates created', async () => {
