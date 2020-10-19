1372965b75dd3178a3229293d39f8d93504c61e6
Davi de Carvalho <davi_carvalho96@hotmail.com>
2020-10-19 18:43:25 -0300

    fix: receiving platform on share course gamefication event

--- src/GameficationModule/controller/gamefication.controller.ts
+++ src/GameficationModule/controller/gamefication.controller.ts
@@ -2,16 +2,12 @@ import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { Constants } from '../../CommonsModule/constants';
 import { StartEventDTO } from '../dto/start-event.dto';
 import { GameficationService } from '../service/gamefication.service';
-import { UserRewardsService } from '../service/user-rewards.service';
 
 @Controller(
   `${Constants.API_PREFIX}/${Constants.API_VERSION_1}/${Constants.GAMEFICATION_ENDPOINT}`,
 )
 export class GameficationController {
-  constructor(
-    private readonly service: GameficationService,
-    private readonly teste: UserRewardsService,
-  ) {}
+  constructor(private readonly service: GameficationService) {}
 
   @Post('start-event')
   @HttpCode(200)
--- src/GameficationModule/repository/achievement.repository.ts
+++ src/GameficationModule/repository/achievement.repository.ts
@@ -3,6 +3,7 @@ import { Achievement } from '../entity/achievement.entity';
 import { Test } from '../../CourseModule/entity/test.entity';
 import { User } from '../../UserModule/entity/user.entity';
 import { EventNameEnum } from '../enum/event-name.enum';
+import { SocialMediaEnum } from '../dto/start-event-share-course.dto';
 
 @EntityRepository(Achievement)
 export class AchievementRepository extends Repository<Achievement> {
@@ -23,14 +24,20 @@ export class AchievementRepository extends Repository<Achievement> {
     );
   }
 
-  public async getSharedCourseByCourseIdAndUserId<T>(
+  public async getSharedCourseByCourseIdAndUserIdAndSocialMedia<T>(
     courseId: string,
     userId: string,
+    socialMedia: SocialMediaEnum,
   ): Promise<Achievement<T>[]> {
-    const params = [userId, EventNameEnum.USER_REWARD_SHARE_COURSE, courseId];
+    const params = [
+      userId,
+      EventNameEnum.USER_REWARD_SHARE_COURSE,
+      courseId,
+      socialMedia,
+    ];
     return this.query(
       `
-        SELECT * from achievement WHERE userId = ? AND eventName = ? AND rule->>"$.courseId" = ?
+        SELECT * from achievement WHERE userId = ? AND eventName = ? AND rule->>"$.courseId" = ? AND AND rule->>"$.platform" = ?
       `,
       params,
     );
