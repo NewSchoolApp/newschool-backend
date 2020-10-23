8361b234f365db75a6b4a51befb1e21ed9aea390
Davi de Carvalho <davi_carvalho96@hotmail.com>
2020-10-23 12:07:44 -0300

    fix: notification entity now extends audit and has a seen property

--- src/NotificationModule/entity/notification.entity.ts
+++ src/NotificationModule/entity/notification.entity.ts
@@ -1,14 +1,20 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { NotificationTypeEnum } from '../enum/notification-type.enum';
 import { User } from '../../UserModule/entity/user.entity';
+import { Audit } from '../../CommonsModule/entity/audit.entity';
 
 @Entity()
-export class Notification<T> {
+export class Notification<T> extends Audit {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @ManyToOne(() => User, (user: User) => user.notifications)
   user: User;
+
+  @Column({
+    default: true
+  })
+  seen: boolean;
 
   @Column({
     type: 'enum',
