86e4387cb29b32d8061dc9a5e95529b0717462cc
Murilo Rodrigues <murilo.cc.uepb@gmail.com>
2020-10-27 22:36:27 -0300

    fix: remove unecessary query

--- src/GameficationModule/service/course-rewards.service.ts
+++ src/GameficationModule/service/course-rewards.service.ts
@@ -55,18 +55,13 @@ export class CourseRewardsService implements OnModuleInit {
     courseId,
     userId,
   }: CompleteCourseRewardDTO): Promise<void> {
-    const completeCourse = this.courseTakenService.getCompletedByUserIdAndCourseId(
+    const completeCourse = await this.courseTakenService.getCompletedByUserIdAndCourseId(
       userId,
       courseId,
     );
-
+    
     if (!completeCourse) return;
-
-    const { user } = await this.courseTakenService.findByUserIdAndCourseId(
-      userId,
-      courseId,
-    );
-
+    const user:User = completeCourse.user;
     const badge = await this.badgeRepository.findByEventNameAndOrder(
       EventNameEnum.COURSE_REWARD_COMPLETE_COURSE,
       1,
