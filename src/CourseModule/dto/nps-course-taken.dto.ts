3a3a53e656f5c87272ea32ed6bb6e454f1da6dba
Davi de Carvalho <davi_carvalho96@hotmail.com>
2020-11-05 21:35:06 -0300

    fix: making feedback optional and rating now has a range

--- src/CourseModule/dto/nps-course-taken.dto.ts
+++ src/CourseModule/dto/nps-course-taken.dto.ts
@@ -1,11 +1,20 @@
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
 
 export class NpsCourseTakenDTO {
   @IsNotEmpty()
   @IsNumber()
+  @Min(0)
+  @Max(10)
   rating: number;
 
-  @IsNotEmpty()
+  @IsOptional()
   @IsString()
-  feedback: string;
+  feedback?: string;
 }
