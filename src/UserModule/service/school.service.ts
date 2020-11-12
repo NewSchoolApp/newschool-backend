7d19c05d481033572c67b7b039e9152704bd27f7
Leonardo <leonardo@pop-os.localdomain>
2020-11-12 13:36:46 -0300

    refactor: implement only name to search schools

--- src/UserModule/service/school.service.ts
+++ src/UserModule/service/school.service.ts
@@ -1,16 +1,26 @@
 import { HttpService, Injectable } from '@nestjs/common';
 import { School, Schools } from '../dto/school.dto';
+import { query } from 'express';
 
 @Injectable()
 export class SchoolService {
   constructor(private http: HttpService) {}
 
   public async getUserSchool(name: string, cityId: string): Promise<School[]> {
+    let query;
+    if (name && cityId) {
+      query = `?nome=${name}&cidade=${cityId}`;
+    } else if (name && !cityId) {
+      query = `?nome=${name}`;
+    } else {
+      query = `?cidade=${cityId}`;
+    }
     const response = await this.http
       .get<Schools>(
-        `http://educacao.dadosabertosbr.com/api/escolas/buscaavancada?nome=${name}&cidade=${cityId}`,
+        `http://educacao.dadosabertosbr.com/api/escolas/buscaavancada${query}`,
       )
       .toPromise();
+    console.log(response);
     const [id, schoolsArray] = response.data;
     return schoolsArray;
   }
