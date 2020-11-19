1c2713b743e82fe09f06c7838c67da40e09aafbd
Davi de Carvalho <davi_carvalho96@hotmail.com>
2020-11-19 00:15:07 -0300

    fix: added comment related stuff to module

--- src/CourseModule/course.module.ts
+++ src/CourseModule/course.module.ts
@@ -28,6 +28,15 @@ import { CourseTakenController } from './controllers/course.taken.controller';
 import { CourseTakenMapper } from './mapper/course-taken.mapper';
 import { CourseTaken } from './entity/course.taken.entity';
 import { CourseTakenRepository } from './repository/course.taken.repository';
+import { CommentRepository } from './repository/comment.repository';
+import { UserHasCommentRepository } from './repository/user-has-comment.repository';
+import { UserLikedCommentRepository } from './repository/user-liked-comment.repository';
+import { CommentController } from './controllers/comment.controller';
+import { CommentMapper } from './mapper/comment.mapper';
+import { CommentService } from './service/comment.service';
+import { Comment } from './entity/comment.entity';
+import { UserHasComment } from './entity/user-has-comment.entity';
+import { UserLikedComment } from './entity/user-liked-comment.entity';
 
 @Module({
   imports: [
@@ -43,6 +52,12 @@ import { CourseTakenRepository } from './repository/course.taken.repository';
       TestRepository,
       CourseTaken,
       CourseTakenRepository,
+      Comment,
+      CommentRepository,
+      UserHasComment,
+      UserHasCommentRepository,
+      UserLikedComment,
+      UserLikedCommentRepository,
     ]),
     MulterModule.register({ dest: './upload' }),
     forwardRef(() => UserModule),
@@ -54,6 +69,7 @@ import { CourseTakenRepository } from './repository/course.taken.repository';
     PartController,
     TestController,
     CourseTakenController,
+    CommentController,
   ],
   providers: [
     CourseService,
@@ -66,6 +82,8 @@ import { CourseTakenRepository } from './repository/course.taken.repository';
     TestMapper,
     CourseTakenService,
     CourseTakenMapper,
+    CommentMapper,
+    CommentService,
   ],
   exports: [
     CourseService,
