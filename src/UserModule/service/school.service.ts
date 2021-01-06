3069f05d857b560c7a424505c4d92c3cc870d37b
Leonardo <oliveirabalsa2@gmail.com>
2021-01-06 16:10:42 -0300

    feat: implement schools endpoints

--- src/UserModule/service/school.service.ts
+++ src/UserModule/service/school.service.ts
@@ -1,25 +1,18 @@
 import { HttpService, Injectable } from '@nestjs/common';
-import { School, Schools } from '../dto/school.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { School } from '../dto/school.dto';
+import { School as SchoolEntity } from '../entity/school.entity';
 
 @Injectable()
 export class SchoolService {
-  constructor(private http: HttpService) {}
+  constructor(
+    private http: HttpService,
+    @InjectRepository(SchoolEntity)
+    private readonly repository: Repository<SchoolEntity>,
+  ) {}
 
-  public async getUserSchool(name: string, cityId: string): Promise<School[]> {
-    let query;
-    if (name && cityId) {
-      query = `?nome=${name}&cidade=${cityId}`;
-    } else if (name && !cityId) {
-      query = `?nome=${name}`;
-    } else {
-      query = `?cidade=${cityId}`;
-    }
-    const response = await this.http
-      .get<Schools>(
-        `http://educacao.dadosabertosbr.com/api/escolas/buscaavancada${query}`,
-      )
-      .toPromise();
-    const [id, schoolsArray] = response.data;
-    return schoolsArray;
+  public async getUserSchool(name: string): Promise<School[]> {
+    return await this.repository.find({ where: { school: name } });
   }
 }
