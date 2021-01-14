098c985a437951b3c477525ef6d1f9aa0affe951
Davi de Carvalho <davi_carvalho96@hotmail.com>
2021-01-14 10:10:15 -0300

    feat: fixing swagger description

--- src/UserModule/controller/user.controller.ts
+++ src/UserModule/controller/user.controller.ts
@@ -262,8 +262,7 @@ export class UserController {
   })
   @ApiNotFoundResponse({ description: 'thrown if user is not found' })
   @ApiUnauthorizedResponse({
-    description:
-      'thrown if there is not an authorization token or if authorization token does not have ADMIN role',
+    description: `throw if there is not an authorization token, if authorization token does not have STUDENT role or if the id param is different from the user id`,
   })
   @UserIdParam('id')
   @UseGuards(StudentGuard)
