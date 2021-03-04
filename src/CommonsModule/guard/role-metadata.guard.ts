2474d17cdd0194aef8222a472c398e387194f9f9
Davi de Carvalho <davi_carvalho96@hotmail.com>
2021-03-04 14:09:44 -0300

    feat: changed roles to string array and securying create notification

--- src/CommonsModule/guard/role-metadata.guard.ts
+++ src/CommonsModule/guard/role-metadata.guard.ts
@@ -1,5 +1,4 @@
 import { CustomDecorator, SetMetadata } from '@nestjs/common';
-import { RoleEnum } from '../../SecurityModule/enum/role.enum';
 
-export const NeedRole = (...roles: RoleEnum[]): CustomDecorator =>
+export const NeedRole = (...roles: string[]): CustomDecorator =>
   SetMetadata('roles', roles);
