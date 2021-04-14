f8bbb052587fafb701c59df0fbc2afb806448c15
Davi de Carvalho <davi_carvalho96@hotmail.com>
2021-04-14 11:57:23 -0300

    feat: added security guard

--- src/CourseModule/controllers/v2/pilar.controller.ts
+++ src/CourseModule/controllers/v2/pilar.controller.ts
@@ -1,8 +1,17 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { Constants } from '../../../CommonsModule/constants';
 import { PilarService } from '../../service/v2/pilar.service';
 import { CMSPilarDTO } from '../../dto/cms-pilar.dto';
+import { NeedRole } from '../../../CommonsModule/guard/role-metadata.guard';
+import { RoleEnum } from '../../../SecurityModule/enum/role.enum';
+import { RoleGuard } from '../../../CommonsModule/guard/role.guard';
 
 @ApiTags('Pilar')
 @ApiBearerAuth()
@@ -12,11 +21,16 @@ import { CMSPilarDTO } from '../../dto/cms-pilar.dto';
 export class PilarController {
   constructor(private readonly service: PilarService) {}
 
+  @Get()
+  @NeedRole(RoleEnum.STUDENT)
+  @UseGuards(RoleGuard)
   public getAll(): Promise<CMSPilarDTO[]> {
     return this.service.getAll();
   }
 
   @Get('/:id')
+  @NeedRole(RoleEnum.STUDENT)
+  @UseGuards(RoleGuard)
   public findById(@Param('id', ParseIntPipe) id: number): Promise<CMSPilarDTO> {
     return this.service.findById(id);
   }
--- src/CourseModule/controllers/v2/trail.controller.ts
+++ src/CourseModule/controllers/v2/trail.controller.ts
@@ -1,8 +1,17 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { Constants } from '../../../CommonsModule/constants';
 import { TrailService } from '../../service/v2/trail.service';
 import { CMSTrailDTO } from '../../dto/cms-trail.dto';
+import { NeedRole } from '../../../CommonsModule/guard/role-metadata.guard';
+import { RoleEnum } from '../../../SecurityModule/enum/role.enum';
+import { RoleGuard } from '../../../CommonsModule/guard/role.guard';
 
 @ApiTags('Pilar')
 @ApiBearerAuth()
@@ -12,11 +21,16 @@ import { CMSTrailDTO } from '../../dto/cms-trail.dto';
 export class TrailController {
   constructor(private readonly service: TrailService) {}
 
+  @Get()
+  @NeedRole(RoleEnum.STUDENT)
+  @UseGuards(RoleGuard)
   public getAll(): Promise<CMSTrailDTO[]> {
     return this.service.getAll();
   }
 
   @Get('/:id')
+  @NeedRole(RoleEnum.STUDENT)
+  @UseGuards(RoleGuard)
   public findById(@Param('id', ParseIntPipe) id: number): Promise<CMSTrailDTO> {
     return this.service.findById(id);
   }
